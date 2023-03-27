6d96fd5f86de8d138cd26fc131c7c888d7a0baf6
Michael <mi27950@mit.edu>
2023-03-26 20:54:23 -0400

    just saving

--- cdpsession.js
+++ cdpsession.js
@@ -13,12 +13,41 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 const puppeteer = require('puppeteer');
+const util = require('node:util');
+
+var lastScriptLoadedTime = new Date().getTime() / 1000;
+
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function contains(array, element) {
+  for (var i = 0; i < array.length; i++) {
+    if (util.isDeepStrictEqual(array[i], element)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+async function set_breakpoints(session, breakpoints) {
+  console.log("setting " + breakpoints.length + " breakpoints.");
+  for (var i = 0; i < breakpoints.length; i++) {
+    var location = breakpoints[i];
+    var scriptId = location.scriptId;
+    var lineNumber = location.lineNumber;
+    var columnNumber = location.columnNumber;
+    try {
+      //console.log("setting breakpoint at " + scriptId + " " + lineNumber + " " + columnNumber);
+      await session.send('Debugger.setBreakpoint', {location: {scriptId: scriptId, lineNumber: lineNumber, columnNumber: columnNumber}});
+    } catch(err) {
+      //console.log(err);
+    }
+  }
+  console.log("Finished setting breakpoints!");
+}
 
 (async() => {
   // Use Puppeteer to launch a browser and open a page. For some reason doesn't work in a sandbox
@@ -28,32 +57,65 @@ function sleep(ms) {
   const session = await page.target().createCDPSession();
   // Get all the breakpoints
   var all_breakpoints = []
-  await page.goto('https://googlechrome.github.io/devtools-samples/debug-js/get-started');
-  async function getScriptSource(x) {
+  await page.goto('http://groups.csail.mit.edu/cap/');
+  async function getAllBreakpoints(x) {
+    lastScriptLoadedTime = new Date().getTime() / 1000;
     const breakpoints = await session.send("Debugger.getPossibleBreakpoints", {start: {scriptId: x.scriptId, lineNumber: 0}});
-    all_breakpoints = all_breakpoints.concat(breakpoints);
+    //console.log(breakpoints);
+    /*
+    for (var i = 0; i < breakpoints.locations.length; i++) {
+      var breakpoint = breakpoints.locations[i];
+      if (!contains(all_breakpoints, breakpoint)) {
+        all_breakpoints.push(breakpoint);
+      }
+    }
+    */
+    all_breakpoints = all_breakpoints.concat(breakpoints.locations);
   }
 
   async function getVars(x) {
-    let objId = x.callFrames[0].scopeChain[0].object.objectId;
+    //console.log(x.callFrames);
+    //debugger has an array of callframes. hardcoded to get the first one.
+    let callFrame = x.callFrames[0];
+    // each callframe can have multiple scopes, which are returned as an array. Get the first one, which is the one for 'updateLabel'
+    let scope = callFrame.scopeChain[0]
+    //scope.object is the object representing that scope. For local, it contains the variables in that scope. Get the objectId for the scope
+    let objId = scope.object.objectId;
+    // get the properties from the runtime, which will return the variable values.
     var objects = await session.send("Runtime.getProperties", {objectId: objId});
-    console.log(objects);
+    for (var i = 0; i < objects.result.length; i++) {
+      obj = objects.result[i];
+      console.log(obj.name, obj.value);
+    }
+    await session.send("Debugger.resume");
   }
   
 
-  session.on('Debugger.scriptParsed', getScriptSource);
+  session.on('Debugger.scriptParsed', getAllBreakpoints);
   session.on('Debugger.paused' , getVars);
  
-  const debugger_enabled = await session.send('Debugger.enable');
-  const runtime_enabled = await session.send('Runtime.enable');
-  await sleep(1000);
-  //console.log(all_breakpoints[1]);
-  ;
-  const breakpoint = await session.send('Debugger.setBreakpoint', {location: {scriptId: '14', lineNumber: 31}})
-  console.log(breakpoint);
-  //const source = await session.send("Debugger.getScriptSource", {scriptId: '12'});
   
-  // Check it out! Fast animations on the "loading..." screen!
+  const debugger_enabled = await session.send('Debugger.enable');
+  //const runtime_enabled = session.send('Runtime.enable');
+  await sleep(5000);
+  while (true) {
+    await sleep(5000);
+    if (new Date().getTime() / 1000 - lastScriptLoadedTime > 5) {
+      break;
+    }
+    
+  }
+  console.log(all_breakpoints.length + " before filtering");
+  all_breakpoints = all_breakpoints.filter((value, index, self) =>
+  index === self.findIndex((t) => (
+    value.scriptId === t.scriptId && value.lineNumber === t.lineNumber && value.columnNumber === t.columnNumber
+  ))
+  )
+  console.log(all_breakpoints.length + " after filtering");
+  set_breakpoints(session, all_breakpoints);
+  //console.log(all_breakpoints[1].locations[0]);
+  //const breakpoint = await session.send('Debugger.setBreakpoint', {location: {scriptId: '14', lineNumber: 31}});
+  //console.log(breakpoint);
   
   //await browser.close();
 })();
